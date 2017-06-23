8db40c0f348bf0062880fced94e989abbd3b52ce
Hong Xiao <hongtao.xiao@unb.ca>
2017-06-23 16:08:45 -0300

    Home Screen: Text Input

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import { View, StatusBar } from 'react-native';
+import React, { Component } from 'react';
+import { StatusBar } from 'react-native';
 
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 
-export default () =>
-  <Container>
-    <StatusBar translucent={false} barStyle="light-content" />
-    <Logo />
-    <View />
-  </Container>;
+import { InputWithButton } from '../components/TextInput';
+
+const TEMP_BASE_CURRENCY = 'USD';
+const TEMP_QUOTE_CURRENCY = 'GBP';
+const TEMP_BASE_PRICE = '100';
+const TEMP_QUOTE_PRICE = '79.74';
+
+class Home extends Component {
+  handleChangeText = () => {
+    console.log('change text');
+  };
+
+  handlePressBaseCurrency = () => {
+    console.log('press base currency');
+  };
+
+  handlePressQuoteCurrency = () => {
+    console.log('press quote currency');
+  };
+
+  render() {
+    return (
+      <Container>
+        <StatusBar translucent={false} barStyle="light-content" />
+        <Logo />
+        <InputWithButton
+          buttonText={TEMP_BASE_CURRENCY}
+          onPress={this.handlePressBaseCurrency}
+          defaultValue={TEMP_BASE_PRICE}
+          keyboardType="numeric"
+          onChangeText={this.handleChangeText}
+        />
+        <InputWithButton
+          editable={false}
+          buttonText={TEMP_QUOTE_CURRENCY}
+          onPress={this.handlePressQuoteCurrency}
+          value={TEMP_QUOTE_PRICE}
+        />
+      </Container>
+    );
+  }
+}
+
+export default Home;
